b2a98511775a28f35f212877a9dc5fbdae2d7676
bwray <bwray@ARUHIR8ADC211-T.aacps.local>
2018-08-30 15:34:04 -0400

    First Commit

---
+++ PeopleSimulator/src/Person.java
@@ -0,0 +1,14 @@
+class Person {
+
+	public int age;
+	public String name;
+	public String birthday;
+	
+	public Person(int i, String name) {
+		this.name = name;
+		this.age = age;
+	}
+
+
+
+}
---
+++ PeopleSimulator/src/Society.java
@@ -0,0 +1,14 @@
+
+
+public class Society {
+
+	public static void main(String[] args) {
+
+		Person hank = new Person(18, "hank");
+		System.out.println("My name is " + hank.name);
+	}
+
+}
+
+
+
