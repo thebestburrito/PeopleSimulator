f80cceab286a213003b9458eb413d90c26f656ea
Brad Wray <bradwray@gmail.com>
2018-10-18 15:31:13 -0400

    Update Person.java

--- PeopleSimulator/src/Person.java
+++ PeopleSimulator/src/Person.java
@@ -1,14 +1,36 @@
 class Person {
+	private int age;
+	private String name;
 
-	public int age;
-	public String name;
-	public String birthday;
-	
-	public Person(int i, String name) {
+	public Person(int age, String name) {
 		this.name = name;
 		this.age = age;
 	}
 
+	public Person() {
+		this.name = "Brad";
+		this.age = 37;
+	}
 
+	public boolean olderThan(int otherAge) {
+		if (age > otherAge) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void hadBirthday() {
+		age++;
+	}
 
 }
