2e37505707ecda76eeff29de9082f1025be6fe26
DiffDoff <m.e.murray41@gmail.com>
2018-12-13 15:26:01 +0000

    fixed date logic

--- PeopleSimulator/src/Time.java
+++ PeopleSimulator/src/Time.java
@@ -21,7 +21,7 @@ class Time {
       incrementMonth();
     } else if ((month == 1 || month == 3 || month == 5 || month == 7 || month ==  8 || month == 10 || month == 12) && day == 32) {
       incrementMonth();
-    } else if ((month != 1 || month != 3 || month != 5 || month != 7 || month != 8 || month != 10 || month != 12) && day == 31) {
+    } else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) {
       incrementMonth();
     }
     if(month == 13){
