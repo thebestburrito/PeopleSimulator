50f53502eb35754c0f8427aa9e21676c653eca84
JaredRoney <jared@farfieldsystems.com>
2018-12-21 15:38:35 +0000

    Assigned happiness based on some factors such as depression

--- PeopleSimulator/src/Main.java
+++ PeopleSimulator/src/Main.java
@@ -7,7 +7,10 @@ class Main {
 
         //gets the name of the person in the people arrayList at position 8000
          peopleTown.oldestPerson();
-        System.out.println(peopleTown.people.get(8000).getName());
+        for(int i = 0; i < peopleTown.population; i++){
+			//the ArrayList method .get(i) is how to get the element at i
+			System.out.println(i + " " + peopleTown.people.get(i).getName() + " age = "+ peopleTown.people.get(i).getAge() + " Depressed = " + peopleTown.people.get(i).getDepressed() + " Happiness = " + peopleTown.people.get(i).getHappiness());
 	}
 
+}
 }
--- PeopleSimulator/src/Person.java
+++ PeopleSimulator/src/Person.java
@@ -1,10 +1,16 @@
 class Person {
 	public int age;
 	private String name;
+    private boolean depressed;
+    private int happiness;
+    public int income;
 
 	public Person(int age, String name) {
 		this.name = name;
 		this.age = age;
+        this.depressed = Math.random() < 0.15;
+        this.happiness = 50;
+        this.income = (int) Math.floor(Math.random() * 1000000);
 	}
 
 	public Person() {
@@ -32,5 +38,19 @@ class Person {
 	public void hadBirthday() {
 		age++;
 	}
-
+    public boolean getDepressed(){
+        if(age < 11){
+            depressed = false;
+        }
+        return depressed;
+    }
+    public int getHappiness(){
+        if(depressed == true && age > 11){
+            happiness = (int) Math.floor(Math.random() * 36);
+            if(income < 11000){
+                happiness -= (int) Math.floor(Math.random() * 6);
+            }
+        }
+        return happiness;
+    }
 }
