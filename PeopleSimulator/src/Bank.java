e7c063a937e202fd0017c76112857c07bffd086a
Miranda <kaniiyo@outlook.com>
2019-01-09 15:35:52 +0000

    Bank/balance and general edits

--- PeopleSimulator/src/Bank.java
+++ PeopleSimulator/src/Bank.java
@@ -1,25 +1,12 @@
 
 class Bank {
-    public double balance;
-    public int debt;
-    public int accountNumber;
+    public int balance;
+    public int withdrawl;
     public int deposit;
 
-public Bank(int accountNumber, double balance){
-    this.accountNumber = accountNumber;
+public Bank(int idNumber, int balance){
+    //this.idNumber = 0;
     this.balance = balance;
 }
 
-    public void updateBalance(){
-
-    }
-    //Will updatethe balance of the account
-    public int getAccountNumber(){
-        return accountNumber;
-    }
-    //Returns the account number for a person
-
-    public double getBalance(){
-        return balance;
-    }
 }
--- PeopleSimulator/src/Main.java
+++ PeopleSimulator/src/Main.java
@@ -1,10 +1,10 @@
 class Main {
 	public static void main(String[] args) {
-    System.out.println("Cheeze!");
 		Society peopleTown = new Society(10000);
 
-      for(int i = 0; i < 10000; i++) {
-        peopleTown.cycleDay();
+      for(int i = 0; i < peopleTown.population; i++) {
+        //peopleTown.cycleDay();
+        System.out.println(i + " " + peopleTown.people.get(i).getName() + " age = "+ peopleTown.people.get(i).getAge() + " Balance = " + peopleTown.people.get(i).getBalance());
       }
 	}
 
