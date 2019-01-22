cb9b8cec420edb5c387cbd0714fa33a5a539222d
AMCGidan <hggbcf055@gmail.com>
2019-01-22 15:45:08 +0000

    Version that will be pulled

--- PeopleSimulator/src/Main.java
+++ PeopleSimulator/src/Main.java
@@ -16,22 +16,15 @@ class Main {
         System.out.println("Day: " + i);
       }
 
-    //   for(Person individual : peopleTown.people){
-    //       //This is a test of the people who got married
-    //       if(individual.getSpouse() != null){
-    //           System.out.println(individual.getName() + " married to " + individual.getSpouse().getName() );
-    //           System.out.println(individual.getSpouse().getName() + " is married to " + individual.getName());
-    //       }
-    //       //Write your print/tests below if they involve a property of the people in peopleTown
-    //       //use individual as it does in the example above
-
-
-
-    //   }
-
-    //     for(int i = 0; i < peopleTown.places.size(); i++){
-    //         System.out.println(peopleTown.places.get(i).getPlace());
-    //     }
+      for(Person individual : peopleTown.people){
+          //This is a test of the people who got married
+          if(individual.getSpouse() != null){
+              System.out.println(individual.getName() + " married to " + individual.getSpouse().getName() );
+              System.out.println(individual.getSpouse().getName() + " is married to " + individual.getName());
+          }
+          //Write your print/tests below if they involve a property of the people in peopleTown
+          //use individual as it does in the example above
+      }
 	}
 
 
