cfe9faf0693d3e8663c540cfa3d15b6387074a7d
Brad Wray <bradwray@gmail.com>
2019-01-24 18:25:39 +0000

    getting it ready for a show and tell

--- PeopleSimulator/src/Main.java
+++ PeopleSimulator/src/Main.java
@@ -1,21 +1,19 @@
 class Main {
 	public static void main(String[] args) {
-        Society peopleTown = new Society(100);
-         for(int i = 0; i < 10000; i++) {
+        Society peopleTown = new Society(20);
+         for(int i = 0; i < 100; i++) {
             peopleTown.cycleDay();
-            //Write your print/tests in here if your feature should happen on a dialy basis
-            p(peopleTown.time.dateToString());
+            //Uncomment any of these to shot them
             if(!peopleTown.getDisaster().equals("none")){
-                //Shows Disater news report
+                //Shows Disaster news report
                 //p(peopleTown.getDisaster() + " on " + peopleTown.time.dateToString());
             }
             if(false){
                 //Jared and Sophia's Societal Happiness
-                p(" Societal Happiness = " + peopleTown.societalHappiness() +"% on "+ peopleTown.time.dateToString());
+                //p(" Societal Happiness = " + peopleTown.societalHappiness() +"% on "+ peopleTown.time.dateToString());
             }
             for(Person individual : peopleTown.people){
                 //Logan's mating and wedlock methods
-
                 if(individual.spouse != null){
                     p(individual.getName() + " married to " + individual.getSpouse().getName() );
                     p(individual.getSpouse().getName() + " is married to " + individual.getName());
@@ -28,7 +26,7 @@ class Main {
 
         for(Person individual : peopleTown.people){
             //Logan's mating and wedlock methods
-            if(individual.getSpouse() != null){
+            if(individual.spouse != null){
                 System.out.println(individual.getName() + " married to " + individual.getSpouse().getName() );
                 System.out.println(individual.getSpouse().getName() + " is married to " + individual.getName());
             }
@@ -36,14 +34,11 @@ class Main {
               System.out.println(individual.getName() + " got divorced " + individual.getNumOfTimesDivorced() + " time(s) because their happiness (or sometimes their spouse's) dropped to " + individual.getHappiness());
             }
 
-            //Justin & Drew's income calculated on the bell curve... maybe too many poor people
             if(individual.getAge() >= 20){
-                //System.out.println(individual.getName() + "'s Income: " + individual.income);
-            }
+                System.out.println(individual.getName() + "'s Income: " + individual.income);
+                System.out.println(individual.getName() + " has personality number " + individual.getPersonalityNumber() + ", called "+ individual.getPersonalityString() + ". " + "isLawful = " + individual.getPersonalityBoolean("isLawful"));
 
-            //Fiona's assigning personality numbers based on Dungeouns and Dragons alignments
-            //System.out.println(individual.getName() + " has personality number " + individual.getPersonalityNumber() + ", called "+ individual.getPersonalityString() + ". " + "isLawful = " + individual.getPersonalityBoolean("isLawful"));
-            //System.out.println(individual.getName() + " has personality number " + individual.getPersonalityNumber() + ", called "+ individual.getPersonalityString() + ". " + "isLawful = " + individual.getPersonalityBoolean("isLawful"));
+            }
 
 
         }
